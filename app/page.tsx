f5f180ad2b7d6f8b8d468beffea1e3100b91faed
Mtoal087 <112990339+Mtoal087@users.noreply.github.com>
2024-01-11 02:39:10 -0600

    Finished mobile, might add box shadowing

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,39 @@
 'use client'
+
 import { useState } from 'react';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0); // Counter, using useState from React
 
-  return (
-    <section className="w-full flex h-screen justify-center items-center bg-slate-600 flex-col gap-8">
-      <h1 className="text-4xl text-blue-400">
-        Counter
-      </h1>
-      <div className="button flex gap-20 sm:flex-col">
-        <button className="btn cursor-pointer hover:bg-red-400 absolute left-20 sm:w-100"
-        onClick={() => setCount(count - 1)}
-        >
-          -
-        </button>
-        <h1>{ count }</h1>
-        <button className="btn cursor-pointer hover:bg-green-400 absolute right-20"
-        onClick={() => setCount(count + 1)}
-        >
-          +
-        </button>
-      </div>
-    </section>
-  )
+    return (
+        <section className="w-full flex h-screen justify-center items-center bg-slate-600 flex-col gap-8">
+
+
+        <h1 className="text-6xl text-blue-400">Counter</h1>
+
+
+        {/* Counter */}
+        <h1 className='text-7xl mb-16 font-bold'>{ count }</h1>
+    
+
+        <div className="flex md:flex-row flex-col w-screen items-center gap-10">
+
+
+            {/* Button 'Add' */}
+            <button className="w-4/5 cursor-pointer text-4xl  border-2 border-green-800 py-3 rounded-xl hover:bg-green-400 font-bold"
+            onClick={() => setCount(count + 1)}
+            >Add</button>
+
+
+            {/* Button 'Subtract' */}
+            <button className="w-4/5 cursor-pointer hover:bg-red-400 text-4xl border-2 border-red-800 py-3 rounded-xl font-bold"
+            onClick={() => setCount(count - 1)}
+            >Subtract</button>
+
+
+        </div>
+
+
+        </section>
+    )
 }

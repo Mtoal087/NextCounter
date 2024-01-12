35f6df3e65a85999b6e301ce5745fa2d163847b0
Mtoal087 <112990339+Mtoal087@users.noreply.github.com>
2024-01-12 02:20:12 -0600

    Finished

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,17 @@ export default function Home() {
         <h1 className='text-7xl mb-16 font-bold'>{ count }</h1>
     
 
-        <div className="flex md:flex-row flex-col w-screen items-center gap-10">
+        <div className="flex sm:flex-row flex-col w-screen items-center gap-10 sm:gap-0">
 
 
             {/* Button 'Add' */}
-            <button className="w-4/5 cursor-pointer text-4xl  border-2 border-green-800 py-3 rounded-xl hover:bg-green-400 font-bold"
+            <button className="w-4/5 cursor-pointer text-4xl  border-4 border-green-700 py-3 rounded-xl hover:bg-green-300 hover:text-slate-800 font-bold shadow-md hover:shadow-2xl sm:mx-12"
             onClick={() => setCount(count + 1)}
             >Add</button>
 
 
             {/* Button 'Subtract' */}
-            <button className="w-4/5 cursor-pointer hover:bg-red-400 text-4xl border-2 border-red-800 py-3 rounded-xl font-bold"
+            <button className="w-4/5 cursor-pointer hover:bg-red-300 text-4xl border-4 border-red-700 py-3 rounded-xl font-bold shadow-md hover:shadow-2xl hover:text-slate-800 sm:mx-12"
             onClick={() => setCount(count - 1)}
             >Subtract</button>
 

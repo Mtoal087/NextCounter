5e46655ee093b4a7c3fdc61aee7b2a424d1cbb8a
Mtoal087 <112990339+Mtoal087@users.noreply.github.com>
2024-01-17 17:59:30 -0600

    Might make button components instead of creating multiple buttons in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function Home() {
 	const [count, setCount] = useState(0); // Counter, using useState from React
 
 	return (
-		<section className="w-full flex h-screen justify-center items-center bg-slate-600 flex-col gap-8">
+		<section className="flex flex-col items-center justify-center w-full h-screen gap-8 bg-slate-600">
 			<h1 className="text-6xl text-blue-400">Counter</h1>
 
 			{/* Counter */}
-			<h1 className="text-7xl mb-16 font-bold">{count}</h1>
+			<h1 className="mb-16 font-bold text-7xl">{count}</h1>
 
-			<div className="flex sm:flex-row flex-col w-screen items-center gap-10 sm:gap-0">
+			<div className="flex flex-col items-center w-screen gap-10 sm:flex-row sm:gap-0">
 				{/* Button 'Add' */}
 				<button
-					className="w-4/5 cursor-pointer text-4xl  border-4 border-green-700 py-3 rounded-xl hover:bg-green-300 hover:text-slate-800 font-bold shadow-md hover:shadow-2xl sm:mx-12 hover:bg-opacity-70"
+					className="w-4/5 py-3 text-4xl font-bold border-4 border-green-700 shadow-md cursor-pointer sm:py-5 sm:hover:bg-green-300 rounded-xl sm:hover:shadow-2xl sm:hover:text-slate-800 sm:mx-12 sm:hover:bg-opacity-70"
 					onClick={() => setCount(count + 1)}
 				>
 					Add
@@ -23,20 +23,20 @@ export default function Home() {
 
 				{/* Button 'Subtract' */}
 				<button
-					className="w-4/5 cursor-pointer hover:bg-red-300 text-4xl border-4 border-red-700 py-3 rounded-xl font-bold shadow-md hover:shadow-2xl hover:text-slate-800 sm:mx-12 hover:bg-opacity-70"
+					className="w-4/5 py-3 text-4xl font-bold border-4 border-red-700 shadow-md cursor-pointer sm:py-5 sm:hover:bg-red-300 rounded-xl sm:hover:shadow-2xl sm:hover:text-slate-800 sm:mx-12 sm:hover:bg-opacity-70"
 					onClick={() => setCount(count - 1)}
 				>
 					Subtract
 				</button>
-
-				{/* Button 'Reset' */}
-				<button
-					className="w-4/5 cursor-pointer hover:bg-yellow-300 text-4xl border-4 border-yellow-400 py-3 rounded-xl font-bold shadow-md hover:shadow-2xl hover:text-slate-800 sm:mx-12 hover:bg-opacity-70"
-					onClick={() => setCount(0)}
-				>
-					Reset
-				</button>
 			</div>
+
+			{/* Button 'Reset' */}
+			<button
+				className="w-4/5 p-3 text-4xl font-bold border-4 border-yellow-400 shadow-md cursor-pointer sm:mt-7 sm:py-5 sm:hover:bg-yellow-300 rounded-xl sm:hover:shadow-2xl sm:hover:text-slate-800 sm:hover:bg-opacity-70"
+				onClick={() => setCount(0)}
+			>
+				Reset
+			</button>
 		</section>
 	);
 }
